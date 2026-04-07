23061637f652164a5c70578418fe08a72e9f4ee3
Manjeet Kumar Gupta <manjeetkumargupta9654@gmail.com>
2026-04-07 20:02:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let heading = document.getElementById("heading");
+let btn = document.getElementById("btn");
+let input = document.getElementById("nameInput");
+let boxes = document.querySelectorAll(".box");
+
+btn.addEventListener("click", function () {
+    let name = input.value;
+
+    if (name === "") {
+        heading.innerText = "Hello";
+    } else {
+        heading.innerText = "Hello " + name;
+    }
+});
+
+boxes.forEach(function (box) {
+    box.addEventListener("click", function () {
+        let color = box.innerText.toLowerCase();
+
+        box.style.backgroundColor = color;
+
+        if (color === "yellow") {
+            box.style.color = "black";
+        } else {
+            box.style.color = "white";
+        }
+    });
+});
